0907704a9af5e11ee3c33722ea2e3b72e103fb82
Mik Fogel <life904@yandex.ru>
2017-09-30 16:06:54 +0300

    что-то начал

---
+++ main.py
@@ -0,0 +1,10 @@
+class client:
+    def __init__(self, action=0):
+        self.action = action
+
+    def set_action(new_action):
+        self.action = new_action
+
+
+class baba:
+    def __init__(self, response)            
